0a1954a2a5804e580ea5587ff78ae8cca0e48a44
97baek <170410_@naver.com>
2021-04-30 01:49:45 +0900

    # createContext와 Provider를 이용해 Context API 구조 잡기

--- react-webgame/8.Minesweeper/Minesweeper.jsx
+++ react-webgame/8.Minesweeper/Minesweeper.jsx
@@ -1,5 +1,28 @@
-import React, { useReducer } from "react";
+import React, { useReducer, createContext, useMemo } from "react";
+import Form from "./Form";
 import Table from "./Table";
+
+export const CODE = {
+  MINE: -7,
+  NORMAL: -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+};
+
+export const TableContext = createContext({
+  // 초기값
+  tableData: [
+    [-1, -1, -1, -1, -1, -1, -1, -1],
+    [-7, -7, -7, -7, -7, -7, -7],
+    [-1, -7, -1, -7, -7, -7, -1],
+    [],
+    [],
+  ],
+  dispatch: () => {},
+});
 
 const initialState = {
   tableData: [],
@@ -7,8 +30,15 @@ const initialState = {
   result: 0,
 };
 
+export const START_GAME = "START_GAME";
+
 const reducer = (state, aciton) => {
   switch (action.type) {
+    case START_GAME:
+      return {
+        ...state,
+        tableData: plantMine(action.row, action.cell, action.mine),
+      };
     default:
       return state;
   }
@@ -16,13 +46,19 @@ const reducer = (state, aciton) => {
 
 const Minesweeper = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  // ContextAPI를 사용하면 렌더링 때 마다 값을 불러와야 하므로 캐싱을 해서 성능 저하를 줄인다.
+  const value = useMemo(() => {
+    tableData: state.tableData, dispatch;
+  }, [state.tableData]);
+
   return (
-    <>
+    <TableContext.Provider value={value}>
       <Form />
       <div>{state.timer}</div>
       <Table />
       <div>{state.result}</div>
-    </>
+    </TableContext.Provider>
   );
 };
 
